6535f3504819546e126349f4a5456a7894240137
radistoubalidis <72709550+radistoubalidis@users.noreply.github.com>
2022-03-22 15:06:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
 # Set random seed
 seed = 42
 
@@ -13,23 +15,25 @@ seed = 42
 
 # Load in the data
 df = pd.read_csv("Iris.csv")
-
+y = df.pop('Species')
 # Split into train and test sections
-y = df.pop("Species")
+
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
+
+df['Species'] = y
 
 #################################
 ########## MODELLING ############
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=2, random_state=seed)
-regr.fit(X_train, y_train)
+clf = RandomForestClassifier(max_depth=2, random_state=seed)
+clf.fit(X_train, y_train)
 
 # Report training set score
-train_score = regr.score(X_train, y_train) * 100
+train_score = clf.score(X_train, y_train) * 100
 # Report test set score
-test_score = regr.score(X_test, y_test) * 100
+test_score = clf.score(X_test, y_test) * 100
 
 # Write scores to a file
 with open("metrics.txt", 'w') as outfile:
@@ -40,8 +44,9 @@ with open("metrics.txt", 'w') as outfile:
 ##########################################
 ##### PLOT FEATURE IMPORTANCE ############
 ##########################################
+
 # Calculate feature importance in random forest
-importances = regr.feature_importances_
+importances = clf.feature_importances_
 labels = df.columns
 feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
 feature_df = feature_df.sort_values(by='importance', ascending=False,)
@@ -56,30 +61,29 @@ ax.set_xlabel('Importance',fontsize = axis_fs)
 ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
 ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
-plt.tight_layout()
+plt.tight_layout()s
 plt.savefig("feature_importance.png",dpi=120) 
 plt.close()
 
 
 ##########################################
-############ PLOT RESIDUALS  #############
+############ PLOT CONF MATRIX  ###########
 ##########################################
 
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+y_pred = clf.predict(X_test)
+res_df = pd.DataFrame(list(zip(y_test,y_pred)), columns = ["true","pred"])
 
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
 
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
+conf_matrix = confusion_matrix(y_test,y_pred)
 
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
+display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=['Iris-setosa', 'Iris-versicolor','Iris-virginica'])
+
+# image formatting
+axis_fs = 18 #fontsize
+title_fs = 22 #fontsize
+sns.set(style="whitegrid")
+
+display.plot()
+plt.savefig('confusion_matrix.png',dpi=120)
+plt.close()
 
